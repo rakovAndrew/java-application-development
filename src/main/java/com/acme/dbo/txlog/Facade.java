98ee92af63ba86312b85536674c9e77a7d9444a1
Andrew <rakovandrew812@gmail.com>
2022-04-12 15:54:32 +0300

    [iteration-1] - passed iteration-1

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,11 +1,57 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
+
+    public static final String PRIMITIVE_PREFIX = "primitive: ";
+    public static final String CHAR_PREFIX = "char: ";
+    public static final String STRING_PREFIX = "string: ";
+    public static final String REFERENCE_PREFIX = "reference: ";
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        consoleLog(decorateMessage(message));
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        consoleLog(decorateMessage(message));
+    }
+
+    public static void log(char message) {
+        consoleLog(decorateMessage(message));
+    }
+
+    public static void log(String message) {
+        consoleLog(decorateMessage(message));
+    }
+
+    public static void log(boolean message) {
+        consoleLog(decorateMessage(message));
+    }
+
+    public static void log(Object message) {
+        consoleLog(decorateMessage(message));
+    }
+
+    private static void consoleLog(String message) {
+        System.out.println(message);
+    }
+
+    private static String decorateMessage(int message) {
+        return PRIMITIVE_PREFIX + message;
+    }
+
+    private static String decorateMessage(char message) {
+        return CHAR_PREFIX + message;
+    }
+
+    private static String decorateMessage(String message) {
+        return STRING_PREFIX + message;
+    }
+
+    private static String decorateMessage(boolean message) {
+        return PRIMITIVE_PREFIX + message;
+    }
+
+    private static String decorateMessage(Object message) {
+        return REFERENCE_PREFIX + message;
     }
 }
